df92d69be730bf3d8882327f26518ec5f254e85e
tarchevsky_i <i.tarchevsky@gmail.com>
2021-08-19 13:04:33 +0300

    Все проекты: добавлена папка css с переносом css файлов в неё

--- dariiskusstvo2/js/script.js
+++ dariiskusstvo2/js/script.js
@@ -1,9 +1,16 @@
 let header = document.querySelector('.header');
+let arrow = document.querySelector('.arrow');
 
 window.onscroll = function() {
    if (window.pageYOffset > 200) {
       header.classList.add('header__fixed');
+      arrow.classList.add('arrow__fixed');
    } else {
       header.classList.remove('header__fixed');
+      arrow.classList.remove('arrow__fixed');
    }
+};
+
+arrow.onclick = function () {
+   window.scrollTo(0, 0);
 };
